9dc41e6e9e608ac6ebc0203d99c1e7fb4b445109
youginil <youginil@outlook.com>
2024-02-25 15:49:48 +0800

    update deps

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import { Component } from "solid-js";
 const Home: Component = () => {
     return (
         <div>
-            <button class="btn btn-primary">Hello</button>
+            <button class="btn btn-primary w-full">Hello</button>
         </div>
     );
 };
